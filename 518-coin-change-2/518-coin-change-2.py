575f2df7375278b87ac2cf8f650a61608e7daf23
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2022-08-28 14:00:53 -0400

    Time: 166 ms (93.24%), Space: 13.9 MB (91.64%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -4,11 +4,15 @@ class Solution:
         
         dp = [0]*(amount+1)
         dp[0] = 1
-        start = coins[0]
-        res= []
+        
+        # here dp should ideally be a 2d dp with amount and individual coin indices
+        # If we use only last amt to calculate current value there are repetititon for eg to make 5 using 1,2,3 if we use amt method we need to 1 + dp(4) or 2+dp(3) here 1,2,2 1,2,2 will be repeated
+        # so we need to use the coins i.e we fill in using coins indices and then do it
+        
         for coin in coins:
             for amt in range(coin,amount+1):
-                dp[amt]+=dp[amt-coin]
-                
+                #print(amt,coin)
+                dp[amt] += dp[amt-coin]
+        
         return dp[-1]
         
