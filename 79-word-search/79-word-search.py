22a879601503f650826e7c763e9d379e94710d80
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2022-09-04 11:58:02 -0400

    Time: 8864 ms (24.10%), Space: 13.9 MB (50.62%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,28 +1,39 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m = len(board)
-        n = len(board[0])
-        wl = len(word)
-        visited =[[False]*n for i in range(m)]
-        currRow = 0 
-        currCol = 0
+        wordN = len(word)
+        n = len(board)
+        m = len(board[0])
+        visited = [[False]*(m) for _ in range(n)]
+         
         
-        def solutionPresent(currRow,currCol, visited,wordIndex):
-            if wordIndex == wl:
-                return True
+        def recurr(startRow,startCol,index,visited):
+            #print(startRow,startCol,index)
             
-            if currRow<0 or currRow>=m or currCol<0 or currCol>=n or board[currRow][currCol]!=word[wordIndex] or visited[currRow][currCol]:
-                return False
+            if index == wordN or (index==wordN-1 and board[startRow][startCol]==word[index]):
+                return True           
             
-            visited[currRow][currCol] = True
-            res = (solutionPresent(currRow+1,currCol, visited,wordIndex+1) or
-                    solutionPresent(currRow-1,currCol, visited,wordIndex+1) or solutionPresent(currRow,currCol+1, visited,wordIndex+1) or solutionPresent(currRow,currCol-1, visited,wordIndex+1))
-            visited[currRow][currCol] = False
+            visited[startRow][startCol] = True
             
-            return res                    
-        for i in range(m):
-            for j in range(n):
-                if board[i][j]==word[0] and solutionPresent(i,j, visited,0):
-                    return True
+            movement = [[-1,0],[1,0],[0,1],[0,-1]]
+            
+            if board[startRow][startCol] == word[index]:
+                
+                for dx,dy in movement:
+                    newX,newY = startRow+dx,startCol+dy
+                    
+                    if newX <0 or newY<0 or newX>=n or newY>=m or visited[newX][newY]:
+                        continue
+                    
+                    if recurr(newX,newY,index+1,visited):
+                        return True
+            visited[startRow][startCol] = False
+            
+            return False
+        
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == word[0]:
+                    if recurr(i,j,0,visited):
+                        return True
+                    
         return False
-            
