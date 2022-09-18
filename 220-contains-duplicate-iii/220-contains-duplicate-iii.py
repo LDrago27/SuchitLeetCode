b6746a66922b08cc9c27ed33adce10b4ee16788e
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2022-09-18 17:22:18 -0400

    Time: 965 ms (22.83%), Space: 40.8 MB (6.09%) - LeetHub

---
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.py
@@ -0,0 +1,37 @@
+import math
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        
+        
+        # bucket sort method
+        # indexDiff +1 size bucket
+        
+        maxBuckets = int(math.ceil((max(nums)-min(nums))/(valueDiff+1)))+1
+        minEle = min(nums)
+        bucket = [-1]*maxBuckets
+        
+        n = len(nums)
+        
+        for i in range(n):
+            
+            bucketNo = (nums[i]-minEle)//(valueDiff+1)
+
+            print(nums[i],bucketNo,bucket[bucketNo])
+            if bucket[bucketNo]!=-1:
+                return True
+            
+            if bucketNo>=1 and abs(bucket[bucketNo-1]!=-1 and abs(nums[i]-nums[bucket[bucketNo-1]])<=valueDiff):
+                return True
+            
+            if bucketNo<maxBuckets-1 and abs(bucket[bucketNo+1]!=-1 and abs(nums[i]-nums[bucket[bucketNo+1]])<=valueDiff):
+                return True
+            
+            bucket[bucketNo] = i
+            print(bucket[bucketNo])
+            if i>=indexDiff:
+                bucketNo = (nums[i-indexDiff]-minEle)//(valueDiff+1)
+                bucket[bucketNo] = -1
+        return False
+            
+            
+            
