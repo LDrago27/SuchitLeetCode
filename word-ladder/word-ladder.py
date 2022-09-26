c43c5b8969b66d1455090d71b6fa320624b60fe1
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2022-09-26 09:30:58 -0400

    Time: 1486 ms, Memory: 15.5 MB - LeetHub

--- word-ladder/word-ladder.py
+++ word-ladder/word-ladder.py
@@ -1,36 +1,38 @@
+from collections import defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        #We will use a combination of dfs and bfs to solve this problem
         
-        # since it is just the shortest we can directly make use of DFS
+        netWordsSet = set()
+        graph = defaultdict(list)
         
-        wordSet = set(wordList)
-        depth = 0
-        
-        if endWord not in wordSet:
-            return 0
-        
+        for word in wordList:
+            netWordsSet.add(word)
+            
         currLevel = {beginWord}
+        depth = 0
+        if endWord not in wordList:
+            return depth
         
         while currLevel:
-            
-            wordSet = wordSet - currLevel
-            
             nextLevel = set()
-            
+            netWordsSet = netWordsSet-currLevel
+            depth+=1
             for word in currLevel:
-                
-                n = len(word)
-                
-                if word == endWord:
-                    return depth+1
-                
+                n = len(word)                
+                if word==endWord:
+                    return depth
+                    
                 for i in range(n):
                     for char in 'abcdefghijklmnopqrstuvwxyz':
                         newWord = word[:i]+char+word[i+1:]
                         
-                        if newWord in wordSet:
+                        if newWord in netWordsSet:
                             nextLevel.add(newWord)
-            depth+=1
+                            graph[newWord].append(word)
             currLevel = nextLevel
-        return 0
+
             
+        return 0
+                            
+                
