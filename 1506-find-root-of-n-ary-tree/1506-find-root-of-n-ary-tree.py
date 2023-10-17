71f4f8ae2d456220d3ebc7a2963f54a6cbaa24a6
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2023-10-16 21:21:41 -0400

    Time: 129 ms (20.46%), Space: 29 MB (12.39%) - LeetHub

---
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def findRoot(self, tree: List['Node']) -> 'Node':
+        
+        parentSet = set()
+        childSet= set()
+        
+        for node in tree:
+            parentSet.add(node)
+            
+            for child in node.children:
+                childSet.add(child)
+                
+        return list(parentSet - childSet)[0]
+        
+        
+        
