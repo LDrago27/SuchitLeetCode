5948533559599f44261f99b84977107322dd0c22
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2023-12-26 20:03:52 -0500

    Time: 37 ms (81.02%), Space: 17.8 MB (6.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,30 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
-        def buildString(left,right,currStr):
-            
-            
-            if right == n and left == n:
-                res.append(currStr)
-                return
-            
-            if left > n or right >n:
-                return
-            
-            if left > right:
-                # We have two options we can put a left bracket or a right bracket
-                # Case 1 : left bracket
-                buildString(left+1,right,currStr+'(')
-                # Case 2 : right bracket
-                buildString(left,right+1,currStr+')')
-            elif left == right:
-                buildString(left+1,right,currStr+'(')
-                # We can add only left bracket
-            return
-        buildString(1,0,'(')
-        return res
-                
-                
-            
-            
         
+        res = []
+        
+        def generate(noLeft, noRight, currStr):
+            
+            if noLeft == noRight == n:
+                res.append(currStr)
+                return 
+            if noLeft > n or noRight > n:
+                return
+
+            if noLeft < n:
+                # We can add a left one
+                generate(noLeft+1,noRight,currStr + "(")
+                
+            if noRight < noLeft:
+                generate(noLeft,noRight+1,currStr + ")")
+        
+        generate(0,0,'')
+        return res
