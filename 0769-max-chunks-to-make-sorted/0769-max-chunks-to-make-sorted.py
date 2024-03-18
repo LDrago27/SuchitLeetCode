350bec2377e2c022b23ca39616613e17f2e51e39
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2024-03-17 19:43:19 -0700

    Time: 32 ms (74.38%), Space: 16.5 MB (88.66%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -1,40 +1,22 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        # Idea we need to terack max and min of each partition if val is beyond that we change the paruition however if for any of the partition if we have a max and min that is less than any of previous then we return bad
+        def mergeInterval(interval1, interval2):
+            max1,min1 = interval1
+            max2,min2 = interval2
 
-            
-        partStack = [[arr[0],arr[0]]]
+            return (max(max1,max2), min(min1,min2))
         
-        n = len(arr)
-        
-        for i in range(1,n):
-            ele = arr[i]
-            if ele >= partStack[-1][1]:
-                partStack.append([ele,ele])
-            
-            else:
-                maxValue, minValue = partStack[-1][1],ele
-                
-                while partStack and ele < partStack[-1][0]:
-                    tmin, tmax = partStack.pop()
-                    maxValue = max(tmax,maxValue)
-                    minValue = min(tmin,minValue)
-                
-                if partStack and partStack[-1][1] <= ele:
-                    partStack.append([ele,maxValue])
-                elif partStack and partStack[-1][1] > ele:
-                    partStack[-1][1] = maxValue
-                else:
-                    partStack.append([ele,maxValue])
-            print(partStack)
-        return len(partStack)
 
-            
-            
-                 
+        stack = []
 
+        for ele in arr:
 
-                
-            
-                
-                
+            currPart = (ele,ele)
+
+            while stack and not (currPart[0] >= stack[-1][0] and currPart[1] >= stack[-1][0]):
+                prevPart = stack.pop()
+                currPart = mergeInterval(prevPart,currPart)
+
+            stack.append(currPart)
+
+        return len(stack) 
