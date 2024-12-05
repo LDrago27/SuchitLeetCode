3b511bba1fda79c2570169da45acf2ef6e3ac908
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2024-12-04 21:51:06 -0800

    Time: 31 ms (34.58%), Space: 26.5 MB (6.30%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # have 2 arrays left hand Prod and Right Side prod exluding the curr Element
         n = len(nums)
-        leftSide = [1]*n
-        rightSide = [1]*n
+        leftProduct = [1]*n
+        rightProduct = [1]*n
         
         for i in range(1,n):
-            leftSide[i] = leftSide[i-1]*nums[i-1]
+            leftProduct[i] = leftProduct[i-1] * nums[i-1]
+            
         for i in range(n-2,-1,-1):
-            rightSide[i] = rightSide[i+1]*nums[i+1]
+            rightProduct[i] = rightProduct[i+1] * nums[i+1]
             
         res = []
         
         for i in range(n):
-            res.append(leftSide[i]*rightSide[i])
+            res.append(leftProduct[i]*rightProduct[i])
             
         return res
-        
+        # Space:O(n) Time : O(n) # No Division operator needed 
