0543d5e1c489848d2dcb8310cf31ae5dd3066184
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2024-12-21 14:53:31 -0800

    Time: 1719 ms (61.10%), Space: 18 MB (11.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,20 @@
+from heapq import heappop,heappush
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        
         n = len(nums)
-        dp = [1]*n
+        dp = [1]*n # themseleves
         
-        
+        # For each current value we need find elememts  smaller than it increment it
+        # O(n^2) -> Time space O(n)
         for i in range(1,n):
+            temp = dp[i]
             for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i],dp[j]+1)
+                if nums[j] < nums[i]:
+                    temp = max(temp,dp[j]+1)
+            dp[i] = max(dp[i],temp)
+        
         return max(dp)
+            
+                
         
