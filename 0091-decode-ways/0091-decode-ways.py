641b37cb340c6b112c54f5af5b2fc51bae8ca6ba
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2025-01-05 18:34:46 -0800

    Time: 0 ms (100%), Space: 17.7 MB (22.55%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,23 +1,22 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        decodeDict = {}
+
+        for i in range(1,26):
+            decodeDict[str(i)] = chr(ord("A") + (i-1))
         
-        # At each index we have two choice either include current element or include both this and next element
-        
+        res = 0
         n = len(s)
-        dp = [0]*(n+1)
-        
-        dp[0],dp[1] = 1, 1
-        
-        if s[0]=='0':
-            return 0
-        
+        dp = [0]*n
+
+        if s[0] != '0':
+            dp[0] = 1
+
         for i in range(1,n):
-            
-            if s[i] != "0":
-                dp[i+1] = dp[i]
-            
-            if s[i-1]!='0' and int(s[i-1:i+1]) <=26:
-                dp[i+1] += dp[i-1]
+            if s[i] != '0':
+                dp[i] += dp[i-1]
+            if s[i-1] != '0' and int(s[i-1:i+1]) <= 26:
+                dp[i] += dp[i-2] if i-2 >= 0 else 1
         
         return dp[-1]
-        
+            
