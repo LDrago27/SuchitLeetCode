fcfdf42ac5cf3ad5181c96e95ff815a4c29dd4dd
Suchit Sahoo <38322563+LDrago27@users.noreply.github.com>
2025-01-31 22:12:15 -0800

    Time: 12 ms (89.55%), Space: 17.8 MB (64.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,30 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        # Idea: create a hashmap that stores the index of occurence of element
-        # if that element occurs again then shift start to the index+1
-        # remove elements between prev start and current index+1
-        
-        res = 0
-        start = 0
+        start,end = 0,0
         n = len(s)
-        indexOccur = {}
-        for i in range(n):
-            ele = s[i]
+        res = 0
+        charMap = {}
+
+        # Idea we slide a window whenever we get something that is ocverlappingh we move the start ot the index where it was firstr occuring
+
+        while end < n:
+
+            currChar = s[end]
+
+            # This char has occured before
+            while (currChar in charMap and charMap[currChar]!=-1) and start<=charMap[currChar]: 
+                charMap[s[start]] = -1
+                start +=1
             
-            if ele not in indexOccur:
-                indexOccur[ele] = i
-            else:
-                # prev Occurance +1
-                newStart = indexOccur[ele] +1
-                
-                # remove stuff between start and newStart
-                
-                for j in range(start,newStart):
-                    indexOccur.pop(s[j])
-                
-                indexOccur[ele] = i
-                start = newStart
-            res = max(res,i-start+1)
+            charMap[currChar] = end
+            res = max(res,end-start+1)
+            end+=1
         
         return res
